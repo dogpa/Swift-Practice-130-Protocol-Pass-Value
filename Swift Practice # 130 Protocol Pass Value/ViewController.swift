cd6c4c241026c300c2d2ccb2e0b599a01fc454ad
Dogpa <dogpa0130@gmail.com>
2022-01-28 20:50:47 +0800

    Initial Commit

---
+++ Swift Practice # 130 Protocol Pass Value/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 130 Protocol Pass Value
+//
+//  Created by Dogpa's MBAir M1 on 2022/1/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
